30b2e21af9fca3258eb9237bfd809c3cf47efb9d
White <82420216+PikennyW@users.noreply.github.com>
2021-06-27 22:49:33 -0500

    Create reglas.js

---
+++ plugins/reglas.js
@@ -0,0 +1,33 @@
+let handler = async m => m.reply(`
+
+*⺀ REGLAS DE USO ⺀*
+
+*1:* No envies *SPAM* a *Lazari*. 
+
+*2:* No llames a *Lazari*
+
+*3:* No satures a *Lazari*
+
+*4:* Nada de insultos hacia *Lazari*
+
+*5:* No Escribas Al Privado De *Lazari*
+
+*6:* No Invites A Grupos A *Lazari*
+
+*«SANCION PERMANENTE SIN DERECHO A APELACIÓN POR CUALQUIER INCUMPLIMIENTO DE LAS REGLAS»*
+
+*Créditos Samu330 Por El Case De Reglas*
+
+*Créditos A Poker Slayer Por El Uso De Sus Reglas*
+
+                                        
+
+`.trim()) 
+
+handler.help = ['reglas']
+
+handler.tags = ['info']
+
+handler.command = /^reglas$/i
+
+module.exports = handler
